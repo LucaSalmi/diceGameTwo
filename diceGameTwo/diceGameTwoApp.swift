fc284b84d11922c183cc7831078ad5229c38d478
Luca Salmi <luca.salmi90@gmail.com>
2022-02-23 10:27:46 +0100

    Initial Commit

---
+++ diceGameTwo/diceGameTwoApp.swift
@@ -0,0 +1,17 @@
+//
+//  diceGameTwoApp.swift
+//  diceGameTwo
+//
+//  Created by Luca Salmi on 2022-02-23.
+//
+
+import SwiftUI
+
+@main
+struct diceGameTwoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
